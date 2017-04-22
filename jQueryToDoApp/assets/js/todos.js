a4cfe0d88fc2ccdcd063bfe5cc0b8f102d026914
Sarad Gyawali <saradgyawali2010@yahoo.com>
2017-04-22 16:51:44 -0700

    jQuery added

--- jQueryToDoApp/assets/js/todos.js
+++ jQueryToDoApp/assets/js/todos.js
@@ -1,33 +1,29 @@
-//Check Off Specific todos by clicking
- $(document).ready(function() {
- $("ul").on("click", "li", function() {
- 	$(this).toggleClass("completed");
- });
+// Check Off Specific Todos By Clicking
 
 
- // Click on X to delete todo
-  $("ul").on("click", "span", function(){
- 	$(this).parent().fadeOut(500,function(){
- 		$(this).remove();
- 	});
- 	event.stopPropagation(); 
- });
-
-
-$("input[type='text']").keypress(function(event){
-	if(event.which === 13) {
-
-		//grabbing new todo from the input
-		var todoText = ($(this).val());
-
-		//create a new li to add to ul
-		$("ul").append("<li><span><i class='fa fa-trash fa-lg'></i></span> " + todoText + "</li>")
-	};
-
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
 });
 
-$(".fa-pencil-square-o").click(function(){
-	$("input[type='text']").fadeToggle();
-})
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle()
 });
 
